349410f97bac78f292a529332c8cac96379c7eb2
Yuliya <yushkova@sputnikfund.ru>
2021-08-10 11:59:00 +0700

    complex filter formation algorithm updated

--- src/GlobalVariables.ts
+++ src/GlobalVariables.ts
@@ -16,35 +16,35 @@ export const decipherJWT = function (token:string) {
 export const setCookie = async function(name:string, token:string) {
   let decipheredJWT = await decipherJWT(token)
   let expirationDate = decipheredJWT.alive_until
-  // document.cookie = name + "=" + token + "; expires=" + expirationDate + ";samesite=strict;secure;"
+  document.cookie = name + "=" + token + "; expires=" + expirationDate + ";samesite=strict;secure;"
 };
 
 export const getCookie = function (cname: string) {
-  // const name = cname + "=";
-  // const decodedCookie = decodeURIComponent(document.cookie);
-  // const cookieParts = decodedCookie.split(';');
-  // for (let i = 0; i < cookieParts.length; i++) {
-  //   let part = cookieParts[i];
-  //   while (part.charAt(0) == ' ') {
-  //     part = part.substring(1);
-  //   }
-  //   if (part.indexOf(name) == 0) {
-  //     return part.substring(name.length, part.length);
-  //   }
-  // }
+  const name = cname + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieParts = decodedCookie.split(';');
+  for (let i = 0; i < cookieParts.length; i++) {
+    let part = cookieParts[i];
+    while (part.charAt(0) == ' ') {
+      part = part.substring(1);
+    }
+    if (part.indexOf(name) == 0) {
+      return part.substring(name.length, part.length);
+    }
+  }
   return "";
 };
 
 export const deleteAllCookies = function () {
-  // let cookies = document.cookie.split(";");
-  // for (let i = 0; i < cookies.length; i++) {
-  //   let cookie = cookies[i];
-  //   let eqPos = cookie.indexOf("=");
-  //   let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-  //   document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-  // }
+  let cookies = document.cookie.split(";");
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i];
+    let eqPos = cookie.indexOf("=");
+    let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  }
 }
 
 export const deleteCookie = function (name:string) {
-  // document.cookie = name + '=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
+  document.cookie = name + '=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
 }
